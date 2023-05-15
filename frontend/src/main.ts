559de234101ca7c934dc8056c66bbc3ae5854c50
hyjwpk <74606714+hyjwpk@users.noreply.github.com>
2023-05-15 08:41:46 +0800

    [frontend] add Login.vue - remove template - install element-plus @element-plus/icons-vue sass - allowJs - add component Icons - add Login.png - change title - change main.css - change router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,19 @@
-import './assets/main.css'
+import "@/assets/css/main.css";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import Icons from "@/components/Icons.vue";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
+app.component("Icons", Icons)
 app.mount('#app')
---
+++ frontend/src/views/Login/Login.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="login">
+        <el-card class="login_center">
+            <template #header>
+                <div class="card_header">
+                    <span>用户登录</span>
+                </div>
+            </template>
+            <el-form :model="loginFormState" :rules="rules" ref="loginFormRef">
+                <el-form-item prop="name">
+                    <el-input v-model.trim="loginFormState.name" maxlength="32" placeholder="请输入账号" clearable>
+                        <template #prefix>
+                            <icons name="User"></icons>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="pwd">
+                    <el-input v-model.trim="loginFormState.pwd" maxlength="16" show-password placeholder="请输入密码" clearable
+                        @keyup.enter.exact="handleLogin">
+                        <template #prefix>
+                            <icons name="Lock"></icons>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" style="width: 100%" :loading="loginFormState.loading" @click="handleLogin">登
+                        录</el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+    </div>
+</template>
+
+<script>
+import { reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+import Icons from "@/components/Icons.vue";
+
+export default {
+    components: { Icons },
+    setup() {
+        const router = useRouter();
+
+        const loginFormState = reactive({
+            name: "",
+            pwd: "",
+            loading: false,
+        });
+
+        const rules = {
+            name: [{ required: true, message: "账号不能为空", trigger: "blur" }],
+            pwd: [
+                { required: true, message: "密码不能为空", trigger: "blur" },
+                { min: 5, max: 16, message: "密码长度为5-16位", trigger: "blur" },
+            ],
+        };
+
+        const loginFormRef = ref();
+
+        const handleLogin = () => {
+            loginFormRef.value.validate(valid => {
+                if (!valid) {
+                    return false;
+                }
+
+                loginFormState.loading = true;
+                router.replace("/");
+            });
+        };
+
+        return { loginFormState, rules, loginFormRef, handleLogin };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+    width: 100vw;
+    height: 100vh;
+    background-image: url("../../assets/img/Login.png");
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+
+    .login_center {
+        width: 396px;
+        height: auto;
+    }
+
+    .card_header {
+        font-size: 18px;
+        text-align: center;
+    }
+}
+</style>
