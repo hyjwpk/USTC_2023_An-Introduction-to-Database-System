b2ec9035d6c858312e19572a209aefb19038bc22
hyjwpk <74606714+hyjwpk@users.noreply.github.com>
2023-05-15 22:42:48 +0800

    [frontend] add multi-level layout

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,11 +1,20 @@
 import Layout from "@/layout/Index.vue";
+import RouteView from "@/components/RouteView.vue";
 
 const layoutMap = [
     {
-        path: "table",
-        name: "DataTable",
-        meta: { title: "数据表格", roles: ["admin"] },
-        component: () => import("@/views/Data/Table.vue")
+        path: "data",
+        name: "Data",
+        component: RouteView,
+        meta: { title: "数据管理", icon: "DataLine" },
+        children: [
+            {
+                path: "table",
+                name: "DataTable",
+                meta: { title: "数据表格", roles: ["admin"] },
+                component: () => import("@/views/Data/Table.vue")
+            }
+        ]
     }
 ];
 
