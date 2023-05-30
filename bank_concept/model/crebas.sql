6030fb93268947492bd4f7b4a62905900be30bda
eriri <1@qq.com>
2023-05-30 17:31:14 +0800

    update insert sql

--- bank_concept/model/crebas.sql
+++ bank_concept/model/crebas.sql
@@ -1,29 +1,24 @@
-create table Account 
-(
-   Accout_ID            integer                        not null,
-   Client_ID            integer                        not null,
-   bank_name            char(30)                       not null,
-   Password             char(20)                       null,
-   Remaining            integer                        null,
-   Open_date            date                           null,
-   constraint PK_ACCOUNT primary key (Accout_ID)
-);
-
+/*==============================================================*/
+/* Table: Client                                                */
+/*==============================================================*/
 create table Client 
 (
-   Client_ID            integer                        not null,
+   Client_ID            char(18)                       not null,
    Real_name            char(30)                       null,
-   Client_phone         integer                        null,
+   Client_phone         char(11)                       null,
    Client_address       char(30)                       null,
    Client_email         char(30)                       null,
    constraint PK_CLIENT primary key (Client_ID)
 );
 
+/*==============================================================*/
+/* Table: Credit_account                                        */
+/*==============================================================*/
 create table Credit_account 
 (
    Accout_ID            integer                        not null,
-   Client_ID            integer                        null,
-   bank_name            char(30)                       null,
+   Client_ID            char(18)                       not null,
+   bank_name            char(30)                       not null,
    Password             char(20)                       null,
    Remaining            integer                        null,
    Open_date            date                           null,
@@ -31,42 +26,32 @@ create table Credit_account
    constraint PK_CREDIT_ACCOUNT primary key clustered (Accout_ID)
 );
 
+/*==============================================================*/
+/* Table: Department                                            */
+/*==============================================================*/
 create table Department 
 (
    depart_no            integer                        not null,
+   ID                   integer                        null,
    bank_name            char(30)                       not null,
-   ID                   integer                        not null,
    depart_name          char(30)                       null,
    depart_type          char(30)                       null,
    constraint PK_DEPARTMENT primary key (depart_no)
 );
 
-create table Manager 
-(
-   ID                   integer                        not null,
-   depart_no            integer                        not null,
-   Dep_depart_no        integer                        null,
-   bank_name            char(30)                       null,
-   name                 char(30)                       null,
-   sex                  char(1)                        null,
-   Person_ID            integer                        null,
-   Phone                integer                        null,
-   Address              char(30)                       null,
-   Salary               integer                        null,
-   Begin_date           date                           null,
-   level                integer                        null,
-   constraint PK_MANAGER primary key clustered (ID)
-);
-
+/*==============================================================*/
+/* Table: Member                                                */
+/*==============================================================*/
 create table Member 
 (
    ID                   integer                        not null,
-   depart_no            integer                        not null,
+   depart_no            integer                        null,
+   Dep_depart_no        integer                        not null,
    bank_name            char(30)                       not null,
    name                 char(30)                       null,
    sex                  char(1)                        null,
-   Person_ID            integer                        null,
-   Phone                integer                        null,
+   Person_ID            char(18)                       null,
+   Phone                char(11)                       null,
    Address              char(30)                       null,
    Salary               integer                        null,
    Begin_date           date                           null,
@@ -74,6 +59,9 @@ create table Member
    constraint PK_MEMBER primary key (ID)
 );
 
+/*==============================================================*/
+/* Table: Pay_status                                            */
+/*==============================================================*/
 create table Pay_status 
 (
    Pay_ID               char(30)                       not null,
@@ -83,11 +71,14 @@ create table Pay_status
    constraint PK_PAY_STATUS primary key (Pay_ID)
 );
 
+/*==============================================================*/
+/* Table: Saving_account                                        */
+/*==============================================================*/
 create table Saving_account 
 (
    Accout_ID            integer                        not null,
-   Client_ID            integer                        null,
-   bank_name            char(30)                       null,
+   Client_ID            char(18)                       not null,
+   bank_name            char(30)                       not null,
    Password             char(20)                       null,
    Remaining            integer                        null,
    Open_date            date                           null,
@@ -95,25 +86,34 @@ create table Saving_account
    constraint PK_SAVING_ACCOUNT primary key clustered (Accout_ID)
 );
 
+/*==============================================================*/
+/* Table: Serve                                                 */
+/*==============================================================*/
 create table Serve 
 (
    ID                   integer                        not null,
-   Client_ID            integer                        not null,
+   Client_ID            char(18)                       not null,
    constraint PK_SERVE primary key clustered (ID, Client_ID)
 );
 
+/*==============================================================*/
+/* Table: credit_unique                                         */
+/*==============================================================*/
 create table credit_unique 
 (
    bank_name            char(30)                       not null,
    Accout_ID            integer                        not null,
-   Client_ID            integer                        not null,
+   Client_ID            char(18)                       not null,
    constraint PK_CREDIT_UNIQUE primary key clustered (bank_name, Accout_ID, Client_ID)
 );
 
+/*==============================================================*/
+/* Table: loan                                                  */
+/*==============================================================*/
 create table loan 
 (
    Loan_ID              integer                        not null,
-   Client_ID            integer                        not null,
+   Client_ID            char(18)                       not null,
    bank_name            char(30)                       not null,
    Loan_total           integer                        null,
    Loan_datw            date                           null,
@@ -121,14 +121,20 @@ create table loan
    constraint PK_LOAN primary key (Loan_ID)
 );
 
+/*==============================================================*/
+/* Table: saving_unique                                         */
+/*==============================================================*/
 create table saving_unique 
 (
    Accout_ID            integer                        not null,
    bank_name            char(30)                       not null,
-   Client_ID            integer                        not null,
+   Client_ID            char(18)                       not null,
    constraint PK_SAVING_UNIQUE primary key clustered (Accout_ID, bank_name, Client_ID)
 );
 
+/*==============================================================*/
+/* Table: sub_bank                                              */
+/*==============================================================*/
 create table sub_bank 
 (
    bank_name            char(30)                       not null,
@@ -137,21 +143,15 @@ create table sub_bank
    constraint PK_SUB_BANK primary key (bank_name)
 );
 
-alter table Account
-   add constraint FK_ACCOUNT_OPEN_SUB_BANK foreign key (bank_name)
+alter table Credit_account
+   add constraint FK_CREDIT_A_OPEN_SUB_BANK foreign key (bank_name)
       references sub_bank (bank_name)
       on update restrict
       on delete restrict;
 
-alter table Account
-   add constraint FK_ACCOUNT_OWN_CLIENT foreign key (Client_ID)
-      references Client (Client_ID)
-      on update restrict
-      on delete restrict;
-
 alter table Credit_account
-   add constraint FK_CREDIT_A_CREDIT_ACCOUNT foreign key (Accout_ID)
-      references Account (Accout_ID)
+   add constraint FK_CREDIT_A_OWN_CLIENT foreign key (Client_ID)
+      references Client (Client_ID)
       on update restrict
       on delete restrict;
 
@@ -162,19 +162,7 @@ alter table Department
       on delete restrict;
 
 alter table Department
-   add constraint FK_DEPARTME_MANAGE2_MANAGER foreign key (ID)
-      references Manager (ID)
-      on update restrict
-      on delete restrict;
-
-alter table Manager
-   add constraint FK_MANAGER_MANAGE_DEPARTME foreign key (depart_no)
-      references Department (depart_no)
-      on update restrict
-      on delete restrict;
-
-alter table Manager
-   add constraint FK_MANAGER_PROMOTE_MEMBER foreign key (ID)
+   add constraint FK_DEPARTME_MANAGE_MEMBER foreign key (ID)
       references Member (ID)
       on update restrict
       on delete restrict;
@@ -186,7 +174,13 @@ alter table Member
       on delete restrict;
 
 alter table Member
-   add constraint FK_MEMBER_WORK_IN_DEPARTME foreign key (depart_no)
+   add constraint FK_MEMBER_MANAGE2_DEPARTME foreign key (depart_no)
+      references Department (depart_no)
+      on update restrict
+      on delete restrict;
+
+alter table Member
+   add constraint FK_MEMBER_WORK_IN_DEPARTME foreign key (Dep_depart_no)
       references Department (depart_no)
       on update restrict
       on delete restrict;
@@ -198,8 +192,14 @@ alter table Pay_status
       on delete restrict;
 
 alter table Saving_account
-   add constraint FK_SAVING_A_SAVE_ACCOUNT foreign key (Accout_ID)
-      references Account (Accout_ID)
+   add constraint FK_SAVING_A_OPEN2_SUB_BANK foreign key (bank_name)
+      references sub_bank (bank_name)
+      on update restrict
+      on delete restrict;
+
+alter table Saving_account
+   add constraint FK_SAVING_A_OWN2_CLIENT foreign key (Client_ID)
+      references Client (Client_ID)
       on update restrict
       on delete restrict;
 
---
+++ bank_concept/model/insert_data.sql
@@ -0,0 +1,42 @@
+INSERT INTO `bank`.`sub_bank` (`bank_name`, `bank_location`, `asset`) VALUES ('xx1分行', '合肥', '20000000');
+INSERT INTO `bank`.`sub_bank` (`bank_name`, `bank_location`, `asset`) VALUES ('xx2分行', '上海', '20000000');
+
+INSERT INTO `bank`.`department` (`depart_no`, `bank_name`, `depart_name`, `depart_type`) VALUES ('1', 'xx1分行', '服务部门', '服务');
+INSERT INTO `bank`.`department` (`depart_no`, `bank_name`, `depart_name`, `depart_type`) VALUES ('2', 'xx1分行', '记账部门', '记账');
+INSERT INTO `bank`.`department` (`depart_no`, `bank_name`, `depart_name`, `depart_type`) VALUES ('3', 'xx2分行', '服务部门', '服务');
+INSERT INTO `bank`.`department` (`depart_no`, `bank_name`, `depart_name`, `depart_type`) VALUES ('4', 'xx2分行', '记账部门', '记账');
+
+INSERT INTO `bank`.`member` (`ID`, `depart_no`, `Dep_depart_no`, `bank_name`, `name`, `sex`, `Person_ID`, `Phone`, `Address`, `Salary`, `Begin_date`, `level`) VALUES ('1', '1', '1', 'xx1分行', 'b', 'M', '100000000000000001', '10000000001', 'abcD1-2-304', '100000', '2019-1-1', '7');
+INSERT INTO `bank`.`member` (`ID`, `depart_no`, `Dep_depart_no`, `bank_name`, `name`, `sex`, `Person_ID`, `Phone`, `Address`, `Salary`, `Begin_date`, `level`) VALUES ('2', '2', '2', 'xx1分行', 'c', 'W', '100000000000000000', '20000000000', 'ac 4-5-607', '100000', '2018-01-01', '8');
+INSERT INTO `bank`.`member` (`ID`, `depart_no`, `Dep_depart_no`, `bank_name`, `name`, `sex`, `Person_ID`, `Phone`, `Address`, `Salary`, `Begin_date`, `level`) VALUES ('3', '3', '3', 'xx2分行', 'x', 'W', '200000000000000000', '12300000001', '下北泽', '900000', '2018-07-01', '7');
+INSERT INTO `bank`.`member` (`ID`, `depart_no`, `Dep_depart_no`, `bank_name`, `name`, `sex`, `Person_ID`, `Phone`, `Address`, `Salary`, `Begin_date`, `level`) VALUES ('4', '4', '4', 'xx2分行', 'y', 'M', '200000000000000001', '12300000002', '编不出来了', '900000', '2018-08-01', '7');
+INSERT INTO `bank`.`member` (`ID`, `Dep_depart_no`, `bank_name`, `name`, `sex`, `Person_ID`, `Phone`, `Address`, `Salary`, `Begin_date`, `level`) VALUES ('100', '1', 'xx1分行', 'a', 'M', '100000000000000011', '10000000000', 'abc1-2-304', '5000', '2023-05-29', '1');
+INSERT INTO `bank`.`member` (`ID`, `Dep_depart_no`, `bank_name`, `name`, `sex`, `Person_ID`, `Phone`, `Address`, `Salary`, `Begin_date`, `level`) VALUES ('200', '4', 'xx2分行', 'z', 'W', '200000000000000022', '12300000000', '电波塔', '6000', '2022-04-02',  '2');
+
+UPDATE `bank`.`department` SET `ID` = '1' WHERE (`depart_no` = '1');
+UPDATE `bank`.`department` SET `ID` = '2' WHERE (`depart_no` = '2');
+UPDATE `bank`.`department` SET `ID` = '3' WHERE (`depart_no` = '3');
+UPDATE `bank`.`department` SET `ID` = '4' WHERE (`depart_no` = '4');
+
+INSERT INTO `bank`.`client` (`Client_ID`, `Real_name`, `Client_phone`, `Client_address`, `Client_email`) VALUES ('123123200203150000', '泽村斯潘塞英梨梨', '11223344556', '我家', 'eriri@qq.com');
+INSERT INTO `bank`.`client` (`Client_ID`, `Real_name`, `Client_phone`, `Client_address`, `Client_email`) VALUES ('234234000000000000', '水上由岐', '12345123456', '泽衣村', 'yuki@qq.com');
+INSERT INTO `bank`.`client` (`Client_ID`, `Real_name`, `Client_phone`, `Client_address`, `Client_email`) VALUES ('280800000000000000', '锦木千束', '12345678901', 'LycoReco', 'chisato@qq.com');
+INSERT INTO `bank`.`client` (`Client_ID`, `Real_name`, `Client_phone`, `Client_address`, `Client_email`) VALUES ('302300000000000000', '井之上泷奈', '12345678909', 'LycoReco', 'takina@qq.com');
+
+INSERT INTO `bank`.`loan` (`Loan_ID`, `Client_ID`, `bank_name`, `Loan_total`, `Loan_datw`, `Loan_rate`) VALUES ('10', '302300000000000000', 'xx1分行', '10000', '2022-11-11', '0.05');
+
+INSERT INTO `bank`.`pay_status` (`Pay_ID`, `Loan_ID`, `Pay_money`, `Pay_date`) VALUES ('11', '10', '1000', '2022-12-12');
+INSERT INTO `bank`.`pay_status` (`Pay_ID`, `Loan_ID`, `Pay_money`, `Pay_date`) VALUES ('12', '10', '3000', '2023-3-1');
+
+INSERT INTO `bank`.`serve` (`ID`, `Client_ID`) VALUES ('1', '123123200203150000');
+INSERT INTO `bank`.`serve` (`ID`, `Client_ID`) VALUES ('3', '234234000000000000');
+INSERT INTO `bank`.`serve` (`ID`, `Client_ID`) VALUES ('100', '280800000000000000');
+INSERT INTO `bank`.`serve` (`ID`, `Client_ID`) VALUES ('100', '302300000000000000');
+
+INSERT INTO `bank`.`credit_account` (`Accout_ID`, `Client_ID`, `bank_name`, `Password`, `Remaining`, `Open_date`, `overdraft`) VALUES ('1', '123123200203150000', 'xx1分行', '123123', '3000', '2023-3-1', '10000');
+
+INSERT INTO `bank`.`credit_unique` (`bank_name`, `Accout_ID`, `Client_ID`) VALUES ('xx1分行', '1', '123123200203150000');
+
+INSERT INTO `bank`.`saving_account` (`Accout_ID`, `Client_ID`, `bank_name`, `Password`, `Remaining`, `Open_date`, `interest_rate`) VALUES ('2', '280800000000000000', 'xx1分行', '123456', '5000', '2023-2-1', '0.03');
+
+INSERT INTO `bank`.`saving_unique` (`Accout_ID`, `bank_name`, `Client_ID`) VALUES ('2', 'xx1分行', '280800000000000000');
