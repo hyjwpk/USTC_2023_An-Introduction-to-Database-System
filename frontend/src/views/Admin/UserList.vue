40181cd0f6b451fb29f711b33e822724a80590a3
hyjwpk <74606714+hyjwpk@users.noreply.github.com>
2023-06-01 22:31:26 +0800

    [frontend] add search

--- frontend/src/views/Admin/UserList.vue
+++ frontend/src/views/Admin/UserList.vue
@@ -4,8 +4,8 @@
             <div class="card_header">
                 <b>用户列表</b>
                 <div>
-                    <el-button color="#056DE8" @click="dialogFormVisible = true">增加用户</el-button>
-                    <el-button color="#056DE8" @click="dialogFormVisible = true">搜索</el-button>
+                    <el-button color="#056DE8" @click="addUserDialogFormVisible = true">增加用户</el-button>
+                    <el-button color="#056DE8" @click="searchDialogFormVisible = true">搜索</el-button>
                 </div>
             </div>
         </template>
@@ -34,19 +34,33 @@
                 @current-change="handleCurrentChange" />
         </div>
 
-        <el-dialog v-model="dialogFormVisible" title="增加用户">
-            <el-form :model="form">
+        <el-dialog v-model="addUserDialogFormVisible" title="增加用户">
+            <el-form :model="addForm">
                 <el-form-item label="用户名" label-width=100px>
-                    <el-input v-model="form.name" autocomplete="off" />
+                    <el-input v-model="addForm.name" autocomplete="off" />
                 </el-form-item>
                 <el-form-item label="密码" label-width=100px>
-                    <el-input v-model="form.password" autocomplete="off" />
+                    <el-input v-model="addForm.password" autocomplete="off" />
                 </el-form-item>
             </el-form>
             <template #footer>
                 <span class="dialog-footer">
-                    <el-button @click="dialogFormVisible = false">取消</el-button>
+                    <el-button @click="addUserDialogFormVisible = false">取消</el-button>
                     <el-button type="primary" @click="handleAdd()">确定</el-button>
+                </span>
+            </template>
+        </el-dialog>
+
+        <el-dialog v-model="searchDialogFormVisible" title="搜索用户">
+            <el-form :model="searchForm">
+                <el-form-item label="用户名" label-width=100px>
+                    <el-input v-model="searchForm.name" autocomplete="off" />
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="searchDialogFormVisible = false">取消</el-button>
+                    <el-button type="primary" @click="handleSearch()">确定</el-button>
                 </span>
             </template>
         </el-dialog>
@@ -62,22 +76,31 @@ import { ElMessage } from "element-plus";
 export default {
     setup() {
         const tableData = ref([])
-        const dialogFormVisible = ref(false);
-        const form = reactive({
+        const addUserDialogFormVisible = ref(false);
+        const searchDialogFormVisible = ref(false);
+        const addForm = reactive({
             name: "",
             password: "",
+        });
+        const searchForm = reactive({
+            name: "",
         });
         const currentPage = ref(1);
         const pageSize = ref(2);
         const count = ref(0);
+        const name = ref("");
 
-        onMounted(() => {
-            request.get("/api/page", { params: { page: currentPage.value, size: pageSize.value } }).then(res => {
+        const load = () => {
+            request.get("/api/page", { params: { page: currentPage.value, size: pageSize.value, name: name.value } }).then(res => {
                 tableData.value = res.data.data;
                 count.value = res.data.count;
             }).catch(err => {
                 ElMessage.error(err);
             });
+        };
+
+        onMounted(() => {
+            load();
         });
 
         const handleEdit = (data) => {
@@ -97,52 +120,50 @@ export default {
             request.post("/api/deleteUser", { name: data.name }).then(res => {
                 if (res.data.code == 0) {
                     ElMessage.success(res.data.message);
+                    load();
                 } else {
                     ElMessage.error(res.data.message);
                 }
             }).catch(err => {
                 ElMessage.error(err);
             });
-            tableData.value = tableData.value.filter(item => item.name != data.name);
         };
 
         const handleSizeChange = (number) => {
             pageSize.value = number;
-            request.get("/api/page", { params: { page: currentPage.value, size: pageSize.value } }).then(res => {
-                tableData.value = res.data.data;
-                count.value = res.data.count;
-            }).catch(err => {
-                ElMessage.error(err);
-            });
+            load();
         };
 
         const handleCurrentChange = (number) => {
             currentPage.value = number;
-            request.get("/api/page", { params: { page: currentPage.value, size: pageSize.value } }).then(res => {
-                tableData.value = res.data.data;
-                count.value = res.data.count;
-            }).catch(err => {
-                ElMessage.error(err);
-            });
+            load();
         };
 
         const handleAdd = () => {
-            request.post("/api/addUser", { name: form.name, password: form.password }).then(res => {
+            request.post("/api/addUser", { name: addForm.name, password: addForm.password }).then(res => {
                 if (res.data.code == 0) {
                     ElMessage.success(res.data.message);
+                    load();
                 } else {
                     ElMessage.error(res.data.message);
                 }
             }).catch(err => {
                 ElMessage.error(err);
             });
-            dialogFormVisible.value = false;
-            tableData.value.push({ name: form.name, password: form.password });
+            addUserDialogFormVisible.value = false;
+        };
+
+        const handleSearch = () => {
+            name.value = searchForm.name;
+            load();
+            searchDialogFormVisible.value = false;
         };
         return {
             tableData,
-            dialogFormVisible,
-            form,
+            addUserDialogFormVisible,
+            searchDialogFormVisible,
+            addForm,
+            searchForm,
             currentPage,
             pageSize,
             count,
@@ -151,6 +172,7 @@ export default {
             handleSizeChange,
             handleCurrentChange,
             handleAdd,
+            handleSearch,
         };
     }
 }
