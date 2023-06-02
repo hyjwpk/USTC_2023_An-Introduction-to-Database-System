86692730f3cafd86a7684fc9c5a096c646254595
hyjwpk <74606714+hyjwpk@users.noreply.github.com>
2023-06-02 10:32:07 +0800

    [backend] change url

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class UserController {
     final
     UserService userService;
--- backend/src/main/java/com/example/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/example/backend/mapper/UserMapper.java
@@ -1,8 +1,7 @@
 package com.example.backend.mapper;
 
 import com.example.backend.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -12,13 +11,13 @@ public interface UserMapper {
     @Select("select name, password from user")
     List<User> getUserList();
 
-    @Select("update user set password = #{password} where name = #{name}")
+    @Update("update user set password = #{password} where name = #{name}")
     void editUser(User user);
 
-    @Select("insert into user(name, password) values(#{name}, #{password})")
+    @Insert("insert into user(name, password) values(#{name}, #{password})")
     void addUser(User user);
 
-    @Select("delete from user where name = #{name}")
+    @Delete("delete from user where name = #{name}")
     void deleteUser(User user);
 
     @Select("select count(*) from user where name like concat('%', #{name}, '%')")
